6344709b7601282f5ccb6ae2863ccb51481ab66b
michal jamry <jamrymi@gmail.com>
2018-01-13 00:31:04 +0100

    dat

--- src/test/java/pl/jamry/michal/neuralnet/example/mnist/ReaderExample.java
+++ src/test/java/pl/jamry/michal/neuralnet/example/mnist/ReaderExample.java
@@ -1,6 +1,8 @@
 package pl.jamry.michal.neuralnet.example.mnist;
 
-import org.junit.Test;
+import org.junit.Before;
+import pl.jamry.michal.neuralnet.tensors.JBlasTensor;
+import pl.jamry.michal.neuralnet.tensors.Tensor;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -11,13 +13,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ReaderExample {
+    Tensor x;
+    Tensor y;
 
-    @Test
-    public void a() throws IOException, URISyntaxException {
+    @Before
+    public void prepareData() throws IOException, URISyntaxException {
         Path path = Paths.get(ClassLoader.getSystemResource("train.csv").toURI());
 
         double[][] doubles = null;
-        double[] labels = null;
+        double[][] labels = null;
 
 
         List<String> lines = Files.lines(path).collect(Collectors.toList());
@@ -27,17 +31,19 @@ public class ReaderExample {
             if (i == 0) {
                 int noOfValues = x.split(",").length - 1;
                 doubles = new double[lines.size() - 1][noOfValues];
-                labels = new double[lines.size() - 1];
+                labels = new double[lines.size() - 1][1];
 
             } else {
                 String label = x.split(",")[0]; //first value is label
-                labels[i - 1] = Double.parseDouble(label);
+                labels[i - 1][0] = Double.parseDouble(label);
 
                 double[] data = str2dblarr(x.substring(label.length() + 1)); //everything after the label is data
                 System.arraycopy(data, 0, doubles[i - 1], 0, data.length);
             }
         }
 
+        x = new JBlasTensor(doubles);
+        y = new JBlasTensor(labels);
     }
 
     private double[] str2dblarr(String string) {
